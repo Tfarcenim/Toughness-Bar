a2737a472d95c7d97260ab4520df7cdca63f7a64
Tfarcenim <44327798+Gimpler@users.noreply.github.com>
2019-12-30 01:24:20 -0500

    1.15 port finished

--- src/main/java/com/tfar/toughnessbar/EventHandlerClient.java
+++ src/main/java/com/tfar/toughnessbar/EventHandlerClient.java
@@ -1,13 +1,10 @@
 package com.tfar.toughnessbar;
 
 import com.mojang.blaze3d.systems.RenderSystem;
-import com.tfar.toughnessbar.ToughnessBar;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.AbstractGui;
-import net.minecraft.client.gui.IngameGui;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.SharedMonsterAttributes;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.MathHelper;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
@@ -17,7 +14,7 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.tfar.toughnessbar.ToughnessBarConfig.ClientConfig.*;
+import static com.tfar.toughnessbar.ToughnessBar.ToughnessBarConfig.ClientConfig.*;
 import static net.minecraft.client.gui.AbstractGui.GUI_ICONS_LOCATION;
 
 public class EventHandlerClient {
@@ -26,7 +23,7 @@ public class EventHandlerClient {
   private static final Minecraft mc = Minecraft.getInstance();
   int lastToughness = 0;
 
-  static Index[] indexes = new Index[]{Index.empty, Index.empty, Index.empty, Index.empty, Index.empty, Index.empty, Index.empty, Index.empty, Index.empty, Index.empty};
+  static Index[] indexes;
 
   @SubscribeEvent(receiveCanceled = true)
   public void onRenderArmorToughnessEvent(RenderGameOverlayEvent.Post event) {
@@ -41,11 +38,11 @@ public class EventHandlerClient {
           colorValues.get().stream().filter(hexColor -> hexColor.startsWith("#")).forEach(hexColor -> colors.add(Integer.parseInt(hexColor.substring(1), 16)));
           if (colors.isEmpty()) {
             //Add white as a default if nothing was loaded from the config. White doesn't change texture color
-            colors.add(0);
+            colors.add(0xffffff);
           }
         }
         if (lastToughness != armorToughness) calculateIndex(armorToughness);
-        int layer = armorToughness / 20;
+        int layer = (int)Math.ceil(armorToughness / 20d) - 1;
         int color = getColor(layer);
         int previous = getColor(layer - 1);
 
@@ -57,18 +54,23 @@ public class EventHandlerClient {
         mc.getTextureManager().bindTexture(TEXTURE);
         for (int i = 0; i < 10; i++) {
           Index index = indexes[i];
-          switch (index){
-            case full:
-            case half:
-            case empty:
+          if (layer > 0)//toughness>20
+          switch (index) {
+            case empty: drawFullIcon(previous,i,right,top);break;
+            case half: drawSplitIcon(previous,color,i,right,top);break;
+            case full: drawFullIcon(color,i,right,top);break;
+          } else {//toughness<=20
+            switch (index) {
+              case empty: if (empty.get()) drawEmptyIcon(color,i,right,top);break;
+              case half: drawHalfIcon(color,i,right,top);break;
+              case full: drawFullIcon(color,i,right,top);break;
+            }
           }
         }
-        0xf000f0;
         ForgeIngameGui.right_height += 10;
 
         //Revert state
         RenderSystem.popMatrix();
-
         mc.getTextureManager().bindTexture(GUI_ICONS_LOCATION);
         RenderSystem.disableBlend();
       }
@@ -76,13 +78,12 @@ public class EventHandlerClient {
   }
 
   void calculateIndex(int armorToughness) {
-    int layer = armorToughness / 20;
-
-    if (layer >= indexes.length) {
-      return;
-    }
     indexes = new Index[]{Index.empty, Index.empty, Index.empty, Index.empty, Index.empty, Index.empty, Index.empty, Index.empty, Index.empty, Index.empty};
     int modulo = armorToughness % 20;
+    if (modulo == 0){
+      indexes = new Index[]{Index.full, Index.full, Index.full, Index.full, Index.full, Index.full, Index.full, Index.full, Index.full, Index.full};
+      return;
+    }
     int fullicons = modulo / 2;
     boolean halficon = armorToughness % 2 == 1;
     for (int i = 0; i < fullicons; i++) {
@@ -95,23 +96,33 @@ public class EventHandlerClient {
   }
 
   private int getColor(int index) {
-    if (index < 0) {
-      return 0xffffff;
-    } else if (index >= colors.size()) {
-      return showBedrock.get() ? 0xffffff : colors.get(colors.size() - 1);
-    }
-    return colors.get(index);
+    return index < 0 ? 0xffffff : index >= colors.size() ? colors.get(colors.size() - 1) : colors.get(index);
   }
 
-
-
-  private void drawIcon(int color, int i,Index index, int guiLeft, int guiTop) {
+  private void drawEmptyIcon(int color, int i, int guiLeft, int guiTop) {
     RenderSystem.color3f((color >> 16 & 0xff) / 256f, (color >> 8 & 0xff) / 256f, (color & 0xff) / 256f);
-    blit(guiLeft, guiTop, 0, index.ordinal(), index.ordinal()+1, 9, 9, 9,9);
+    blit(guiLeft - i * 8, guiTop, 18,27, 9, 9);
   }
 
-  public static void blit(int x, int y, int z, float u, float v, int width, int height) {
-    blit(x,y,z,u,v,width,height,9,9);
+  private void drawFullIcon(int color, int i, int guiLeft, int guiTop) {
+    RenderSystem.color3f((color >> 16 & 0xff) / 256f, (color >> 8 & 0xff) / 256f, (color & 0xff) / 256f);
+    blit(guiLeft- i * 8, guiTop, 9, 18, 9, 9);
+  }
+
+  private void drawHalfIcon(int color, int i, int guiLeft, int guiTop) {
+    RenderSystem.color3f((color >> 16 & 0xff) / 256f, (color >> 8 & 0xff) / 256f, (color & 0xff) / 256f);
+    if (empty.get())drawEmptyIcon(color,i,guiLeft,guiTop);
+    blit(guiLeft - i * 8, guiTop, 0, 9, 9, 9);
+  }
+
+  private void drawSplitIcon(int color1,int color2, int i, int guiLeft, int guiTop) {
+    drawFullIcon(color1,i,guiLeft,guiTop);
+    RenderSystem.color3f((color2 >> 16 & 0xff) / 256f, (color2 >> 8 & 0xff) / 256f, (color2 & 0xff) / 256f);
+    blit(guiLeft - i * 8, guiTop, 0, 9, 9, 9);
+  }
+
+  public static void blit(int x, int y, float u, float v, int width, int height) {
+    blit(x,y,0,u,v,width,height,9,27);
   }
 
   public static void blit(int x, int y, int z, float u, float v, int width, int height, int textureX, int textureY) {
@@ -119,7 +130,6 @@ public class EventHandlerClient {
   }
 
     enum Index {
-    half, full, empty,
-    halfcap, cap
+    half, full, empty
   }
 }
--- src/main/java/com/tfar/toughnessbar/ToughnessBar.java
+++ src/main/java/com/tfar/toughnessbar/ToughnessBar.java
@@ -1,5 +1,7 @@
 package com.tfar.toughnessbar;
 
+import com.google.common.collect.Lists;
+import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
@@ -7,23 +9,54 @@ import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.List;
 
 import static com.tfar.toughnessbar.ToughnessBar.MOD_ID;
 
 @Mod(value = MOD_ID)
 public class ToughnessBar {
-    public static final String MOD_ID = "toughnessbar";
-    public static Logger logger = LogManager.getLogger(MOD_ID);
+  public static final String MOD_ID = "toughnessbar";
 
-    public ToughnessBar(){
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, ToughnessBarConfig.CLIENT_SPEC);
+  public ToughnessBar() {
+    FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+    ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, ToughnessBarConfig.CLIENT_SPEC);
+  }
+
+  public void setup(FMLClientSetupEvent event) {
+    MinecraftForge.EVENT_BUS.register(new EventHandlerClient());
+  }
+
+  public static class ToughnessBarConfig {
+
+    public static final ClientConfig CLIENT;
+    public static final ForgeConfigSpec CLIENT_SPEC;
+
+    static {
+      final Pair<ClientConfig, ForgeConfigSpec> specPair = new ForgeConfigSpec.Builder().configure(ClientConfig::new);
+      CLIENT_SPEC = specPair.getRight();
+      CLIENT = specPair.getLeft();
     }
 
-    public void setup(FMLClientSetupEvent event) {
-        MinecraftForge.EVENT_BUS.register(new EventHandlerClient());
-    }
+    public static class ClientConfig {
+      public static ForgeConfigSpec.BooleanValue empty;
+      public static ForgeConfigSpec.ConfigValue<List<? extends String>> colorValues;
 
+      ClientConfig(ForgeConfigSpec.Builder builder) {
+        builder.push("general");
+        colorValues = builder
+                .comment("Toughness Bar Icon Colors")
+                .translation("text.toughnessbar.config.colorvalues")
+                .defineList("color values", Lists.newArrayList("#FFFFFF", "#FF5500", "#FFC747", "#27FFE3", "#00FF00", "#7F00FF"), String.class::isInstance);
+        empty = builder
+                .comment("Show empty armor toughness icons?")
+                .translation("text.toughnessbar.config.showemptyarmortoughnessicons")
+                .define("Show empty icons", false);
+        builder.pop();
+      }
+    }
+  }
 }
